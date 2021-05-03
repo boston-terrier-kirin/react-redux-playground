f978b31a83ffd42df435480bedc444597eac30f4
kirin <kohei.matsumoto@gmail.com>
2021-05-04 06:42:18 +0900

    redux-logger

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,8 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import logger from 'redux-logger';
 import rootReducer from './root-reducer';
 
-const store = createStore(rootReducer);
+const middlewares = [logger];
+const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
 export default store;
